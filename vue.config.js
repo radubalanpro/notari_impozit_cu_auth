c3e8bc3c899e579797f827eb0a648f35f2f97626
radubalanpro <radubalanpro@yahoo.com>
2022-08-02 00:04:45 +0300

    cateva actualizari

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://radubalanpro.github.io/notari_impozit_cu_auth/'
+    ? '/notari_impozit_cu_auth/'
     : '/'
 })
